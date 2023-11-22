be759ff81447c534b9fc3fab55691b0db480d95e
Klurifixus <144474109+Klurifixus@users.noreply.github.com>
2023-11-22 12:10:16 +0100

    #2 test path: urls

--- django_project/earn_project/earn_project/urls.py
+++ django_project/earn_project/earn_project/urls.py
@@ -25,11 +25,28 @@ from django.contrib.auth.views import LoginView
  
 
 urlpatterns = [
-    path('', home, name='home'),
-    path('forum/', views.forum, name='forum'),
-    path('login/', LoginView.as_view(), name='login'),
-    path('register/', views.register, name='register'),
-    path('archive/', LoginView.as_view(), name='archive'),
-    path('about/', LoginView.as_view(), name='about'),
-    path('contact/', LoginView.as_view(), name='contact'),
-]
+path('', home, name='home'),
+path('forum/', views.forum, name='forum'),
+path('login/', LoginView.as_view(), name='login'),
+path('register/', views.register, name='register'),
+path('archive/', LoginView.as_view(), name='archive'),
+path('about/', LoginView.as_view(), name='about'),
+path('contact/', LoginView.as_view(), name='contact'),
+] 
+# from django.contrib import admin
+# from django.urls import path, include
+# from .views import handler403, handler404, handler405, handler500
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('', include('home.views')),
+#     path('about/', include('about.views')),
+#     path('contact/', include('contact.urls')),
+#     path('accounts/', include('allauth.urls')),
+#     path('summernote/', include('django_summernote.urls')),
+# ]
+
+# handler403 = 'movieHub.views.handler403'
+# handler404 = 'movieHub.views.handler404'
+# handler405 = 'movieHub.views.handler405'
+# handler500 = 'movieHub.views.handler500'
